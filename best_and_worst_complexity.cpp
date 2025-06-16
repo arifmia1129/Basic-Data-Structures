19d1f354a43a49c5783104ee8e8555314c5d247d
Md. Arif <arifmia1129@gmail.com>
2025-06-16 20:32:15 +0600

    Best to Worst Complexity

---
+++ best_and_worst_complexity.cpp
@@ -0,0 +1,20 @@
+/**
+ Suppose -> N = 1000
+ * Constant -> O(1) = 1
+ * Linear -> O(N) = 1000
+ * Logarithmic -> O(logN) = 10
+ * Square Root -> O(sqrt(N)) = 32
+ * Quadratic -> O(N * N) = 1000000
+ * Linearithmic -> O(NLogN) = 1000 * 10
+ * */ 
+
+
+//  Sort by best complexity
+/**
+ * Constant
+ * Logarithmic
+ * Square Root
+ * Linear
+ * Linearithmic
+ * Quadratic
+ * */  
