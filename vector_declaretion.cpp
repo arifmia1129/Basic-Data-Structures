d2311be18cae376e2b5dbdb1e7a25363e9f0b9d9
Md. Arif <arifmia1129@gmail.com>
2025-06-18 06:18:43 +0600

    vector capacity method

--- vector_declaretion.cpp
+++ vector_declaretion.cpp
@@ -11,8 +11,25 @@ int main () {
 
     vector<int> v2(a, a + 5); //type4
 
+    // cout << "Vector size: " << v2.size() << endl;
+    // cout << "Vector max size: " << v2.max_size() << endl;
+    // cout << "Vector capacity " << v2.capacity() << endl;
+    // v2.push_back(19);
+    // cout << "Vector capacity " << v2.capacity() << endl;
+
+
+
     // vector<int> v2 = {1, 2, 3, 4};
 
+    // v2.clear();
+
+    v2.resize(2);
+
+    if(v2.empty()) {
+        cout << "There is no element in above array." << endl;
+    }
+
+    cout << v2[0];
     for(int i = 0; i < v2.size(); i++) {
         cout << v2[i] << " ";
     }
