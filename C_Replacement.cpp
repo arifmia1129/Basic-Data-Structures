7607525091b90880f4bfca1a1b10858faeeba388
Md. Arif <arifmia1129@gmail.com>
2025-06-19 06:39:40 +0600

    Extra problem solved

---
+++ C_Replacement.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n;
+
+    cin >> n;
+
+    vector<int> v;
+
+    for(int i = 0; i < n; i++) {
+        int x;
+
+        cin >> x;
+
+        if(x > 0) {
+            v.push_back(1);
+        }else if(x < 0) {
+            v.push_back(2);
+        }else {
+            v.push_back(x);
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
