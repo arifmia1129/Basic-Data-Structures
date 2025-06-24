62296fe52d90fec8e91f8354c4462c1dc7c6148b
Md. Arif <arifmia1129@gmail.com>
2025-06-24 09:01:45 +0600

    Duplication found

---
+++ duplicate_found.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n;
+
+    cin >> n;
+
+    vector<int> v(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    int is_found = 0;
+
+      for(int i = 0; i < n; i++) {
+        if(v[i] == v[i + 1]) {
+            is_found = 1;
+            break;
+        }
+    }
+
+    if(is_found) {
+        cout << "Duplicate found" << endl; 
+    }else {
+        cout << "Duplicate not found" << endl;
+    }
+
+    return 0;
+}
