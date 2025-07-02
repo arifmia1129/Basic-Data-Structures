4efa5f64fb3d9ebac6de601595ab820db08561d1
Md. Arif <arifmia1129@gmail.com>
2025-07-02 17:53:11 +0600

    Insertion at Head

--- module_6_practice_problems.cpp
+++ module_6_practice_problems.cpp
@@ -58,6 +58,67 @@
 
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Node {
+//     public:
+//         int val;
+//         Node* next;
+    
+//     Node(int val) {
+//         this->val = val;
+//         this->next = NULL;
+//     }
+// };
+
+// void countElement(Node* head) {
+//     Node* tmp = head;
+
+//     int sumOfCount = 0;
+//     while(tmp != NULL) {
+//         sumOfCount++;
+//         tmp = tmp->next;
+//     }
+
+//     cout << sumOfCount;
+// }
+
+// int main () {
+//     int n;
+
+//     cin >> n;
+
+
+//     if(n == 0) {return 0;};
+
+
+//     int val;
+
+//     cin >> val;
+
+
+//     Node* head = new Node(val);
+//     Node* currentNode = head;
+
+//     for(int i = 2; i <= n; i++) {
+//         int val;
+
+//         cin >> val;
+
+//         Node* newNode = new Node(val);
+
+//         currentNode->next = newNode;
+
+//         currentNode = newNode;
+//     }
+
+//     countElement(head);
+    
+//     return 0;
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -72,16 +133,21 @@ class Node {
     }
 };
 
-void countElement(Node* head) {
+void print_list(Node* head) {
     Node* tmp = head;
 
-    int sumOfCount = 0;
     while(tmp != NULL) {
-        sumOfCount++;
+        cout << tmp->val << endl;
+
         tmp = tmp->next;
     }
+}
 
-    cout << sumOfCount;
+void insertAtHead(Node* &head, int val) {
+    Node* newNode = new Node(val);
+
+    newNode->next = head;
+    head = newNode;
 }
 
 int main () {
@@ -113,7 +179,12 @@ int main () {
         currentNode = newNode;
     }
 
-    countElement(head);
+    int insertVal;
+    cin >> insertVal;
+
+    insertAtHead(head, insertVal);
+
+    print_list(head);
     
     return 0;
 }
