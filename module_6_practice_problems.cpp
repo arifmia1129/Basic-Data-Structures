65efcbe4ea8c43d2a425ff4898e83dfaa49ec49f
Md. Arif <arifmia1129@gmail.com>
2025-07-02 17:50:05 +0600

    Count total linked list elements

--- module_6_practice_problems.cpp
+++ module_6_practice_problems.cpp
@@ -1,3 +1,63 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Node {
+//     public:
+//         int val;
+//         Node* next;
+    
+//     Node(int val) {
+//         this->val = val;
+//         this->next = NULL;
+//     }
+// };
+
+// void print_list(Node* head) {
+//     Node* tmp = head;
+
+//     while(tmp != NULL) {
+//         cout << tmp->val << endl;
+
+//         tmp = tmp->next;
+//     }
+// }
+
+// int main () {
+//     int n;
+
+//     cin >> n;
+
+
+//     if(n == 0) {return 0;};
+
+
+//     int val;
+
+//     cin >> val;
+
+
+//     Node* head = new Node(val);
+//     Node* currentNode = head;
+
+//     for(int i = 2; i <= n; i++) {
+//         int val;
+
+//         cin >> val;
+
+//         Node* newNode = new Node(val);
+
+//         currentNode->next = newNode;
+
+//         currentNode = newNode;
+//     }
+
+//     print_list(head);
+    
+//     return 0;
+// }
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -12,14 +72,16 @@ class Node {
     }
 };
 
-void print_list(Node* head) {
+void countElement(Node* head) {
     Node* tmp = head;
 
+    int sumOfCount = 0;
     while(tmp != NULL) {
-        cout << tmp->val << endl;
-
+        sumOfCount++;
         tmp = tmp->next;
     }
+
+    cout << sumOfCount;
 }
 
 int main () {
@@ -51,7 +113,7 @@ int main () {
         currentNode = newNode;
     }
 
-    print_list(head);
+    countElement(head);
     
     return 0;
 }
