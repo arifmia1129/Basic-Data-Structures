645c43d0ef3b064168632ab08acf0dd6ba55ccbd
Md. Arif <arifmia1129@gmail.com>
2025-07-02 17:57:32 +0600

    Insertion at Tail

--- module_6_practice_problems.cpp
+++ module_6_practice_problems.cpp
@@ -119,6 +119,77 @@
 // }
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Node {
+//     public:
+//         int val;
+//         Node* next;
+    
+//     Node(int val) {
+//         this->val = val;
+//         this->next = NULL;
+//     }
+// };
+
+// void print_list(Node* head) {
+//     Node* tmp = head;
+
+//     while(tmp != NULL) {
+//         cout << tmp->val << endl;
+
+//         tmp = tmp->next;
+//     }
+// }
+
+// void insertAtHead(Node* &head, int val) {
+//     Node* newNode = new Node(val);
+
+//     newNode->next = head;
+//     head = newNode;
+// }
+
+// int main () {
+//     int n;
+
+//     cin >> n;
+
+
+//     if(n == 0) {return 0;};
+
+
+//     int val;
+
+//     cin >> val;
+
+
+//     Node* head = new Node(val);
+//     Node* currentNode = head;
+
+//     for(int i = 2; i <= n; i++) {
+//         int val;
+
+//         cin >> val;
+
+//         Node* newNode = new Node(val);
+
+//         currentNode->next = newNode;
+
+//         currentNode = newNode;
+//     }
+
+//     int insertVal;
+//     cin >> insertVal;
+
+//     insertAtHead(head, insertVal);
+
+//     print_list(head);
+    
+//     return 0;
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -143,11 +214,11 @@ void print_list(Node* head) {
     }
 }
 
-void insertAtHead(Node* &head, int val) {
+void insertAtTail(Node* &tail, int val) {
     Node* newNode = new Node(val);
 
-    newNode->next = head;
-    head = newNode;
+    tail->next = newNode;
+    tail - newNode;
 }
 
 int main () {
@@ -166,6 +237,7 @@ int main () {
 
     Node* head = new Node(val);
     Node* currentNode = head;
+    Node* tail = head;
 
     for(int i = 2; i <= n; i++) {
         int val;
@@ -177,12 +249,16 @@ int main () {
         currentNode->next = newNode;
 
         currentNode = newNode;
+
+        if(i == n) {
+            tail = currentNode;
+        }
     }
 
     int insertVal;
     cin >> insertVal;
 
-    insertAtHead(head, insertVal);
+    insertAtTail(tail, insertVal);
 
     print_list(head);
     
