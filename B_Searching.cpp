eb1d1178eef665a2a66c133cab1809dc1ac775e8
Md. Arif <arifmia1129@gmail.com>
2025-07-03 17:49:38 +0600

    Search problem with linked list

---
+++ B_Searching.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node* next;
+    
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insertAtTail (Node* &head, Node* &tail, int val) {
+    Node* newNode = new Node(val);
+
+    if(head == NULL) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void printList(Node* head) {
+    Node* tmp = head;
+
+    while(tmp != NULL) {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+
+int main () {
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        int val;
+
+        cin >> val;
+
+        insertAtTail(head, tail, val);
+    }
+
+    int q;
+
+    cin >> q;
+    
+
+    Node* tmp = head;
+
+    int idx = 0;
+    bool isExist = false;
+
+    while(tmp != NULL) {
+        if(tmp->val == q) {
+            isExist = true;
+            break;
+        }
+        tmp = tmp->next;
+        idx++;
+    }
+
+    if(isExist) {
+        cout << idx << endl;
+    }else {
+        cout << -1 << endl;
+    }
+    
+    return 0;
+}
