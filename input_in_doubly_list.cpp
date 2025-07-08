5b286b0f88175744afeeff9ebe5cda4f4a6c72fd
Md. Arif <arifmia1129@gmail.com>
2025-07-08 18:31:07 +0600

    Taking input for doubly linked list

---
+++ input_in_doubly_list.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+    
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insertAtTail(Node* &head, Node* &tail, int val) {
+    Node* newNode = new Node(val);
+
+    if(head == NULL) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    newNode->prev = tail;
+
+    tail = newNode;
+}
+
+void printForwardList(Node* head) {
+    Node* tmp = head;
+
+    while(tmp != NULL) {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+int main () {
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int val;
+
+    while(1) {
+        cin >> val;
+        if(val == -1) {
+            break;
+        }
+        
+        insertAtTail(head, tail, val);
+    }
+
+    printForwardList(head);
+    
+    return 0;
+}
