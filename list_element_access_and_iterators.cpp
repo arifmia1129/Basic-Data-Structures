309e377c04f391d1dd866cf1e1849223fdefba67
Md. Arif <arifmia1129@gmail.com>
2025-07-09 07:29:01 +0600

    List element access and iterators

---
+++ list_element_access_and_iterators.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    list<int> l = {10, 20, 30, 40, 50};
+
+    cout << l.front() << endl;
+    cout << l.back() << endl;
+
+    cout << *l.begin() << endl;
+
+    return 0;
+}
