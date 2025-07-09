ffa294b93203afd6786a64fe490e410e9c2c4e38
Md. Arif <arifmia1129@gmail.com>
2025-07-09 07:12:03 +0600

    modifiers functions

--- std_list.cpp
+++ std_list.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main () {
-    list<int> l = {10, 20, 30};
+    list<int> l = {10, 20, 30, 20, 20, 60, 20};
 
     // list<int> l2(l);
 
@@ -35,14 +35,28 @@ int main () {
 
     // l2.assign(l.begin(), l.end());
 
-    l.push_back(40);
-    l.push_front(0);
+    // l.push_back(40);
+    // l.push_front(0);
 
     // cout << *next(l.begin(), 2) << endl;
 
-    list<int> new_list = {11, 22, 33};
+    // list<int> new_list = {11, 22, 33};
 
-    l.insert(next(l.begin(), 2), new_list.begin(), new_list.end());
+    // l.insert(next(l.begin(), 2), new_list.begin(), new_list.end());
+
+
+    // l.erase(next(l.begin(), 2), next(l.begin(), 5));
+
+    // replace(l.begin(), l.end(), 20, 100);
+
+    auto it = find(l.begin(), l.end(), 10);
+
+    if(it != l.end()) {
+        cout << "Found" << endl;
+    }else {
+        cout << "Not found" << endl;
+    }
+
 
     for(int v : l) {
         cout << v << " ";
