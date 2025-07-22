1bf8f2e5a5b86f56eed3a6b05fad2926f872cec1
Md. Arif <arifmia1129@gmail.com>
2025-07-22 21:50:29 +0600

    problem 3 solved

--- practice_day_stack_queue.cpp
+++ practice_day_stack_queue.cpp
@@ -56,6 +56,64 @@
 // }
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main () {
+//     int n;
+
+//     cin >> n;
+
+//     stack<int> s1;
+
+//     while(n--) {
+//         int x;
+
+//         cin >> x;
+
+//         s1.push(x);
+//     }
+
+//     int m;
+
+//     cin >> m;
+
+
+//     if(m != s1.size()) {
+//         cout << "NO" << endl;
+//     }else{
+//         queue<int> q;
+
+//         while(m--) {
+//             int x;
+
+//             cin >> x;
+
+//             q.push(x);
+//         }
+
+//         bool isSame = true;
+
+//         while(!s1.empty()) {
+//             if(s1.top() != q.front()) {
+//                 isSame = false;
+//                 break;
+//             }
+//             s1.pop();
+//             q.pop();
+//         }
+
+//         if(isSame) {
+//             cout << "YES" << endl;
+//         }else {
+//             cout << "NO" << endl;
+//         }
+//     }
+
+//     return 0;
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -74,41 +132,21 @@ int main () {
         s1.push(x);
     }
 
-    int m;
+    stack<int> s2;
 
-    cin >> m;
+    while(!s1.empty()) {
+        int val = s1.top();
+        s2.push(val);
 
-
-    if(m != s1.size()) {
-        cout << "NO" << endl;
-    }else{
-        queue<int> q;
-
-        while(m--) {
-            int x;
-
-            cin >> x;
-
-            q.push(x);
-        }
-
-        bool isSame = true;
-
-        while(!s1.empty()) {
-            if(s1.top() != q.front()) {
-                isSame = false;
-                break;
-            }
-            s1.pop();
-            q.pop();
-        }
-
-        if(isSame) {
-            cout << "YES" << endl;
-        }else {
-            cout << "NO" << endl;
-        }
+        s1.pop();
     }
+
+    while (!s2.empty())
+    {
+        cout << s2.top() << endl;
+        s2.pop();
+    }
+    
 
     return 0;
 }
