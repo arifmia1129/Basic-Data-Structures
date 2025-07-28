517514db0a1866fcd28976094077a95a86590621
Md. Arif <arifmia1129@gmail.com>
2025-07-28 21:51:44 +0600

    Count leaf nodes in a binary tree

---
+++ count_leaf_nodes_in_a_binary_tree.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node* left;
+        Node* right;
+    
+    Node(int val) {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* input() {
+    int val;
+
+    cin >> val;
+
+    Node* root = new Node(val);
+
+    queue<Node*> q;
+
+    q.push(root);
+
+    while(!q.empty()) {
+        Node* parent = q.front();
+        q.pop();
+
+        int left, right;
+
+        cin >> left >> right;
+
+        
+        Node *myLeft, *myRight;
+        
+        if(left == -1) myLeft = NULL;
+        else myLeft = new Node(left);
+
+        if(right == -1) myRight = NULL;
+        else myRight = new Node(right);
+        
+        parent->left = myLeft;
+        parent->right = myRight;
+
+        if(parent->left) q.push(parent->left);
+        if(parent->right) q.push(parent->right);
+    }
+
+    return root;
+}
+
+int count_leaf_node(Node* root) {
+    if(root == NULL)
+        return 0;
+
+    if(root->left == NULL && root->right == NULL) return 1;
+
+    int left = count_leaf_node(root->left);
+    int right = count_leaf_node(root->right);
+
+    return left + right;
+}
+
+int main () {
+    Node* root = input();
+
+    cout << count_leaf_node(root);
+
+    return 0;
+}
